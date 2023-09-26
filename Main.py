74fe66fd426b0c66e920d02118feb6ea9fe82b23
NicolajRB <34749332+NRB11@users.noreply.github.com>
2023-09-26 11:00:53 +0200

    1

--- Main.py
+++ Main.py
@@ -2,8 +2,21 @@ import cv2
 import numpy as np
 
 game = cv2.imread("pictures/1.jpg")
-cv2.imshow("test",game)
 
+height = 20
+width = 20
+hypo = 90
+
+cx = int(height / 2)
+cy = int(width / 2)
+
+start_point = cx - hypo, cy - hypo
+end_point = cx + hypo, cy + hypo
+
+
+pixel_center_bgr = game[cx, cy]
+b, g, r = int(pixel_center_bgr[0]), int(pixel_center_bgr[1]), int(pixel_center_bgr[2])
+cv2.rectangle(game, start_point, end_point, (0, 0, 255), 3)
 
 Higth = 250
 Width = 150
@@ -15,5 +28,6 @@ print(B)
 print(G)
 print(R)
 
+cv2.imshow("test",game)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
