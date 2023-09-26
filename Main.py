0316c066a7ab89a45a9908c957e13a05cdd64e92
NicolajRB <34749332+NRB11@users.noreply.github.com>
2023-09-26 10:52:26 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,4 +3,17 @@ import numpy as np
 
 game = cv2.imread("pictures/1.jpg")
 cv2.imshow("test",game)
+
+
+Higth = 250
+Width = 150
+R = game[Higth,Width][2]
+G = game[Higth,Width][1]
+B = game[Higth,Width][0]
+print(game[Higth,Width])
+print(B)
+print(G)
+print(R)
+
 cv2.waitKey(0)
+cv2.destroyAllWindows()
