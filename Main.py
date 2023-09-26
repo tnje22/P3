78b441d7ccbfa42c636b8095403ceec0c501e670
NicolajRB <34749332+NRB11@users.noreply.github.com>
2023-09-26 13:10:20 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,16 +9,16 @@ Square6 = img2[100:200,0:100]; Square7 = img2[100:200,100:200]; Square8 = img2[1
 
 
 
-Average1 = np.average(Square1,axis=0); colorAverage1 = np.average(Average1,axis=0); print('Average =',colorAverage1)
-Average2 = np.average(Square2,axis=0); colorAverage2 = np.average(Average2,axis=0); print('Average =',colorAverage2)
-Average3 = np.average(Square3,axis=0); colorAverage3 = np.average(Average3,axis=0); print('Average =',colorAverage3)
-Average4 = np.average(Square4,axis=0); colorAverage4 = np.average(Average4,axis=0); print('Average =',colorAverage4)
-Average5 = np.average(Square5,axis=0); colorAverage5 = np.average(Average5,axis=0); print('Average =',colorAverage5)
-Average6 = np.average(Square6,axis=0); colorAverage6 = np.average(Average6,axis=0); print('Average =',colorAverage6)
-Average7 = np.average(Square7,axis=0); colorAverage7 = np.average(Average7,axis=0); print('Average =',colorAverage7)
-Average8 = np.average(Square8,axis=0); colorAverage8 = np.average(Average8,axis=0); print('Average =',colorAverage8)
-Average9 = np.average(Square9,axis=0); colorAverage9 = np.average(Average9,axis=0); print('Average =',colorAverage9)
-Average10 = np.average(Square10,axis=0); colorAverage10 = np.average(Average10,axis=0); print('Average =',colorAverage10)
+Average1 = np.average(Square1,axis=0); colorAverage1 = np.average(Average1,axis=0); print('Average =',colorAverage1[0])
+Average2 = np.average(Square2,axis=0); colorAverage2 = np.average(Average2,axis=0); print('Average =',colorAverage2[0])
+Average3 = np.average(Square3,axis=0); colorAverage3 = np.average(Average3,axis=0); print('Average =',colorAverage3[0])
+Average4 = np.average(Square4,axis=0); colorAverage4 = np.average(Average4,axis=0); print('Average =',colorAverage4[0])
+Average5 = np.average(Square5,axis=0); colorAverage5 = np.average(Average5,axis=0); print('Average =',colorAverage5[0])
+Average6 = np.average(Square6,axis=0); colorAverage6 = np.average(Average6,axis=0); print('Average =',colorAverage6[0])
+Average7 = np.average(Square7,axis=0); colorAverage7 = np.average(Average7,axis=0); print('Average =',colorAverage7[0])
+Average8 = np.average(Square8,axis=0); colorAverage8 = np.average(Average8,axis=0); print('Average =',colorAverage8[0])
+Average9 = np.average(Square9,axis=0); colorAverage9 = np.average(Average9,axis=0); print('Average =',colorAverage9[0])
+Average10 = np.average(Square10,axis=0); colorAverage10 = np.average(Average10,axis=0); print('Average =',colorAverage10[0])
 
 
 cv2.imshow('test1', Square1); cv2.imshow('test2', Square2); cv2.imshow('test3', Square3); cv2.imshow('test4', Square4); cv2.imshow('test5', Square5)
