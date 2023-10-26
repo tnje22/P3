87a609e40f389bc0015f845df482179ef083faca
NRB11 <34749332+NRB11@users.noreply.github.com>
2023-10-26 16:06:52 +0200

    Update Human hand in video V3.py

--- Nicolaj/Human hand in video V3.py
+++ Nicolaj/Human hand in video V3.py
@@ -1,19 +1,15 @@
 import cv2
 import numpy as np
 
-# Function to filter skin color
-def filter_skin_color(frame):
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+# Function to remove a white background
+def remove_white_background(frame):
+    lower_white = np.array([200, 200, 200])
+    upper_white = np.array([255, 255, 255])
+    mask = cv2.inRange(frame, lower_white, upper_white)
+    result = cv2.bitwise_and(frame, frame, mask=~mask)
+    return result
 
-    lower_skin = np.array([0, 20, 70], dtype=np.uint8)
-    upper_skin = np.array([20, 255, 255], dtype=np.uint8)
-
-    skin_mask = cv2.inRange(hsv, lower_skin, upper_skin)
-    filtered_frame = cv2.bitwise_and(frame, frame, mask=skin_mask)
-
-    return filtered_frame
-
-# Function to detect the hand
+# Function to detect the human hand
 def detect_hand(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     _, thresholded = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
@@ -26,9 +22,21 @@ def detect_hand(frame):
     else:
         return None
 
+# Function to filter skin color
+def filter_skin_color(frame):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_skin = np.array([0, 20, 70], dtype=np.uint8)
+    upper_skin = np.array([20, 255, 255], dtype=np.uint8)
+
+    skin_mask = cv2.inRange(hsv, lower_skin, upper_skin)
+    filtered_frame = cv2.bitwise_and(frame, frame, mask=skin_mask)
+
+    return filtered_frame
+
 # Main function
 def main():
-    video_capture = cv2.VideoCapture(video_file_path)  # Replace 'your_video.mp4' with the video filename
+    video_capture = cv2.VideoCapture('C:/Users/Nicol/OneDrive/Skrivebord/Lego Building Videos/%HOMEDRIVE%HOMEPATH%movement1.mkv')
 
     while video_capture.isOpened():
         ret, frame = video_capture.read()
@@ -36,6 +44,7 @@ def main():
         if not ret:
             break
 
+        frame = remove_white_background(frame)
         filtered_frame = filter_skin_color(frame)
         hand_coords = detect_hand(filtered_frame)
 
@@ -58,8 +67,5 @@ def main():
     video_capture.release()
     cv2.destroyAllWindows()
 
-if __name__ != "__main__":
+if __name__ != "__main":
     main()
-
-
-video_file_path = "C:/Users/Nicol/OneDrive/Skrivebord/Lego Building Videos/%HOMEDRIVE%HOMEPATH%movement1.mkv"
