760eff05791144d20d2f6aa6d761cb6dd2b987de
tnje22 <113020850+tnje22@users.noreply.github.com>
2023-11-20 15:03:17 +0100

    new

--- Thomas/step cheker/v1.py
+++ Thomas/step cheker/v1.py
@@ -1,10 +1,9 @@
 import cv2
 import numpy as np
-
+# der kommer en liste af ting der er gjort 
 def process_camera_input(camera_input):
     # Add your Lego bricks and their positions processing
-    #arr=[]
-    #arr[farve][shape]
+
 
     return detected_legos
 
@@ -13,19 +12,7 @@ def identify_current_step(detected_legos):
    #"C:\Users\tnj70\Desktop\Data\Step list dumi.txt"
     num_detected_legos = len(detected_legos)
 
-    if num_detected_legos == 0:
-        print("No Lego bricks detected. Waiting for the first step.")
-    else:
-        print(f"Detected {num_detected_legos} Lego bricks.")
-
-        # Your logic to determine the current step based on the number of detected_legos
-        if num_detected_legos <= 5:
-            print("Step 1: Placing the base bricks.")
-        elif num_detected_legos <= 10:
-            print("Step 2: Adding additional bricks.")
-        else:
-            print("Step 3: Finalizing the Lego structure.")
-
+   
 if __name__ == "__main__":
     camera_input_path = "camera_image.jpg"  # Replace with the actual path to the camera input image
 
