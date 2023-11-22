8ab3433c86c12077a48dd3d24bc9de822ee056d6
NicolajRB <34749332+NRB11@users.noreply.github.com>
2023-11-22 09:45:34 +0100

    Update Invisibility cloak.py

--- Nicolaj/Invisibility cloak.py
+++ Nicolaj/Invisibility cloak.py
@@ -11,10 +11,10 @@ hands = mpHands.Hands()
 
 while True:
     success, img = cap.read()
-    
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    
     results = hands.process(imgRGB)
+    print(results.multi_hand_landmarks)
+    
     
     cv2.imshow("Hand Tracking", img)
     cv2.waitKey(1)
