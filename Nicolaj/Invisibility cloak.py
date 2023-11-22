9e733aa6dafce853a5119332fcc9f10d703cfd3d
NicolajRB <34749332+NRB11@users.noreply.github.com>
2023-11-22 10:01:42 +0100

    Update Invisibility cloak.py

--- Nicolaj/Invisibility cloak.py
+++ Nicolaj/Invisibility cloak.py
@@ -8,9 +8,9 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
-
 pTime = 0
 cTime = 0
+
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -25,7 +25,7 @@ while True:
     fps = 1/(cTime-pTime)
     pTime = cTime
     
-    cv2.putText(img,str(int(fps))
+    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
     
     cv2.imshow("Hand Tracking", img)
     cv2.waitKey(1)
