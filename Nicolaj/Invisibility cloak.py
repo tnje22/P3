7a86143e38db20e02d306987860af22ebe493bb8
NicolajRB <34749332+NRB11@users.noreply.github.com>
2023-11-22 10:29:34 +0100

    Update Invisibility cloak.py

--- Nicolaj/Invisibility cloak.py
+++ Nicolaj/Invisibility cloak.py
@@ -1,9 +1,12 @@
+
+#RUN this to get the mediapipe: pip install mediapipe
+
 import cv2
 import time
 import numpy as np
 import mediapipe as mp
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("C:/Users/Nicol/OneDrive/Skrivebord/Lego Building Videos/building_1.mkv")
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
@@ -19,6 +22,14 @@ while True:
     
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                #print(id,lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+                if id == 0:
+                    cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+                    
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
     
     cTime = time.time()
