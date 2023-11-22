0392c6c8c6bac1501f07571735c0194d0ea3323d
NicolajRB <34749332+NRB11@users.noreply.github.com>
2023-11-22 14:05:56 +0100

    Update Pose Tracking.py

--- Nicolaj/Pose Tracking.py
+++ Nicolaj/Pose Tracking.py
@@ -1,9 +1,13 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import time
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
+
+pTime = 0
+cTime = 0
 
 cap = cv2.VideoCapture(0)
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
@@ -21,7 +25,12 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
         
         
-        
+        cTime = time.time()
+        fps = 1/(cTime-pTime)
+        pTime = cTime
+    
+        #draws the fps on screen
+        cv2.putText(image, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
         
         cv2.imshow('Mediapipe Feed',image)
         
