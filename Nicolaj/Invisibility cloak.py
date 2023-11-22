0496ed05e01ea3a82d223cadeb5f71ef42f98d28
NicolajRB <34749332+NRB11@users.noreply.github.com>
2023-11-22 09:42:55 +0100

    Update Invisibility cloak.py

--- Nicolaj/Invisibility cloak.py
+++ Nicolaj/Invisibility cloak.py
@@ -5,8 +5,16 @@ import mediapipe as mp
 
 cap = cv2.VideoCapture(0)
 
+mpHands = mp.solutions.hands
+
+hands = mpHands.Hands()
+
 while True:
     success, img = cap.read()
+    
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    
+    results = hands.process(imgRGB)
     
     cv2.imshow("Hand Tracking", img)
     cv2.waitKey(1)
