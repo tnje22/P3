6f79c4de992f47bea98847b71072ff2de74c2ef3
NicolajRB <34749332+NRB11@users.noreply.github.com>
2023-11-22 09:57:59 +0100

    Update Invisibility cloak.py

--- Nicolaj/Invisibility cloak.py
+++ Nicolaj/Invisibility cloak.py
@@ -4,13 +4,13 @@ import numpy as np
 import mediapipe as mp
 
 cap = cv2.VideoCapture(0)
-
 mpHands = mp.solutions.hands
-
 hands = mpHands.Hands()
-
 mpDraw = mp.solutions.drawing_utils
 
+
+pTime = 0
+cTime = 0
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -19,7 +19,13 @@ while True:
     
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+    
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    
+    cv2.putText(img,str(int(fps))
     
     cv2.imshow("Hand Tracking", img)
     cv2.waitKey(1)
