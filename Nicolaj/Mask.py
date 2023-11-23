97996cb86273fe5dd5956206fdca3c82b8a43382
NRB11 <34749332+NRB11@users.noreply.github.com>
2023-11-23 22:26:39 +0100

    Update Mask.py

--- Nicolaj/Mask.py
+++ Nicolaj/Mask.py
@@ -10,8 +10,6 @@ image1 = cv2.imread(image_path)
 new_Elbow = Pose_Tracking.ElBow
 new_Wrist = Pose_Tracking.WrIst
 
-cv2.circle(image1, tuple(np.multiply(new_Elbow, [1920, 1080]).astype(int)), 50, (255, 255, 255), cv2.FILLED)
-cv2.circle(image1, tuple(np.multiply(new_Wrist, [1920, 1080]).astype(int)), 50, (255, 255, 255), cv2.FILLED)
-
-cv2.imshow("test frame", image1)
-cv2.waitKey(0)
+print("Elbow x/y ", new_Elbow, "Wrist x/y ", new_Wrist)
+#cv2.imshow("test frame", image1)
+#cv2.waitKey(0)
