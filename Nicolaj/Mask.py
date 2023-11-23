b6424cf18f41cf49cf09b70eb600111aaf6d0eee
NRB11 <34749332+NRB11@users.noreply.github.com>
2023-11-23 21:57:05 +0100

    renamed + update to mask.ppy

--- Nicolaj/Mask.py
+++ Nicolaj/Mask.py
@@ -2,4 +2,10 @@ import cv2
 import numpy as np
 import mediapipe as mp
 import time
-from subprocess import call
+from Pose_Tracking import WristXY, ElbowXY
+
+image_path = 'test.png'
+image1 = cv2.imread(image_path)
+
+while True:
+ print(WristXY, ElbowXY)
