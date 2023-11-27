10c0cd30599e8073d8176642c9a43b290e6fb439
NicolajRB <34749332+NRB11@users.noreply.github.com>
2023-11-27 10:56:39 +0100

    Update Pose test.py

--- Nicolaj/Pose test.py
+++ Nicolaj/Pose test.py
@@ -16,7 +16,7 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         ret, frame = cap.read()
 
         # Path and process for mask image
-        image_path = 'Nicolaj/Images/1920x1080-black-solid-color-background.jpg'
+        image_path = 'Nicolaj/Images/Nicolaj\Images\1920x1080-black-solid-color-background.jpg'
         image1 = cv2.imread(image_path)
 
         # Recolor image to RGB
@@ -34,29 +34,55 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         try:
             landmarks = results.pose_landmarks.landmark
 
-            WristCx = int(landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x * image.shape[1])
-            WristCy = int(landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y * image.shape[0])
+            WristCx = int(landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x * image.shape[1])
+            WristCy = int(landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y * image.shape[0])
+            ElbowCx = int(landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x * image.shape[1])
+            ElbowCy = int(landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y * image.shape[0])
 
-            # Putting the x&y coords in an array
+            # Putting the x&y coords in arrays
             WristXY = [WristCx, WristCy]
+            ElbowXY = [ElbowCx, ElbowCy]
 
             # Assuming box size is 100x100 pixels
             box_size = (100, 100)
 
-            # Define the region of interest (ROI) based on the box size
-            roi = image1[WristXY[1]:WristXY[1] + box_size[1], WristXY[0]:WristXY[0] + box_size[0]]
+            # Calculate the starting point for the region of interest (ROI) for WristXY
+            wrist_roi_start_x = max(0, WristXY[0] - box_size[0] // 2)
+            wrist_roi_start_y = max(0, WristXY[1] - box_size[1] // 2)
+
+            # Define the region of interest (ROI) for WristXY based on the box size
+            wrist_roi = image1[wrist_roi_start_y:wrist_roi_start_y + box_size[1], wrist_roi_start_x:wrist_roi_start_x + box_size[0]]
 
             # Resize the overlay image to match the box size
-            resized_overlay = cv2.resize(roi, (box_size[0], box_size[1]))
+            resized_wrist_overlay = cv2.resize(wrist_roi, (box_size[0], box_size[1]))
 
             # Create a mask using the alpha channel of the overlay image (if exists)
-            mask = resized_overlay[:, :, 3] / 255.0 if resized_overlay.shape[2] == 4 else None
+            wrist_mask = resized_wrist_overlay[:, :, 3] / 255.0 if resized_wrist_overlay.shape[2] == 4 else None
 
-            # Blend the images using the mask
-            blended_roi = cv2.addWeighted(roi, 1 - mask, resized_overlay[:, :, :3], mask, 0) if mask is not None else roi
+            # Blend the images using the mask for WristXY
+            blended_wrist_roi = cv2.addWeighted(wrist_roi, 1 - wrist_mask, resized_wrist_overlay[:, :, :3], wrist_mask, 0) if wrist_mask is not None else wrist_roi
 
-            # Update the frame with the blended ROI
-            image[WristXY[1]:WristXY[1] + box_size[1], WristXY[0]:WristXY[0] + box_size[0]] = blended_roi
+            # Update the frame with the blended ROI for WristXY
+            image[wrist_roi_start_y:wrist_roi_start_y + box_size[1], wrist_roi_start_x:wrist_roi_start_x + box_size[0]] = blended_wrist_roi
+
+            # Calculate the starting point for the region of interest (ROI) for ElbowXY
+            elbow_roi_start_x = max(0, ElbowXY[0] - box_size[0] // 2)
+            elbow_roi_start_y = max(0, ElbowXY[1] - box_size[1] // 2)
+
+            # Define the region of interest (ROI) for ElbowXY based on the box size
+            elbow_roi = image1[elbow_roi_start_y:elbow_roi_start_y + box_size[1], elbow_roi_start_x:elbow_roi_start_x + box_size[0]]
+
+            # Resize the overlay image to match the box size
+            resized_elbow_overlay = cv2.resize(elbow_roi, (box_size[0], box_size[1]))
+
+            # Create a mask using the alpha channel of the overlay image (if exists)
+            elbow_mask = resized_elbow_overlay[:, :, 3] / 255.0 if resized_elbow_overlay.shape[2] == 4 else None
+
+            # Blend the images using the mask for ElbowXY
+            blended_elbow_roi = cv2.addWeighted(elbow_roi, 1 - elbow_mask, resized_elbow_overlay[:, :, :3], elbow_mask, 0) if elbow_mask is not None else elbow_roi
+
+            # Update the frame with the blended ROI for ElbowXY
+            image[elbow_roi_start_y:elbow_roi_start_y + box_size[1], elbow_roi_start_x:elbow_roi_start_x + box_size[0]] = blended_elbow_roi
 
         except:
             pass
